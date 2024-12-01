b80c30175eab02004f3991ffc04e3bc2d1e2fe86
Tetiana Zakharchenko <smyarga@gmail.com>
2024-12-01 10:15:41 +0200

    Day 1. Improve documentation

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,20 +1,53 @@
 '''Advent of Code. Day 1'''
 
 
-def read_file(pathname: str) -> list:
-    '''read'''
+def read_file(pathname: str) -> tuple[list]:
+    '''Read a file and return a tuple of two lists.
+
+    The file should contain pairs of integers separated by spaces, one pair per line.
+    Each integer pair is split into two lists: left and right.
+
+    Args:
+        pathname (str): The path to the file to be read.
+
+    Returns:
+        tuple[list]: A tuple containing two lists of integers.
+    '''
     with open(pathname, 'r', encoding='utf-8') as file:
         left, right = zip(*(map(int, line.strip().split()) for line in file))
-        return sorted(left), sorted(right)
+        return left, right
 
 
 def find_diff_id(left: list, right: list) -> int:
-    '''find difference between elements of two lists'''
-    return sum(abs(right[i]-val) for i, val in enumerate(left))
+    '''Find the total difference between elements of two lists.
+
+    The function pairs up the smallest number in the left list with the smallest number
+    in the right list, the second-smallest with the second-smallest, and so on. It then
+    calculates the absolute difference for each pair and returns the sum of these differences.
+
+    Args:
+        left (list): The first list of integers.
+        right (list): The second list of integers.
+
+    Returns:
+        int: The total difference between the paired elements of the two lists.
+    '''
+    return sum(abs(l_val - r_val) for l_val, r_val in zip(sorted(left), sorted(right)))
 
 
 def find_similarity(left: list, right: list) -> int:
-    '''find similarity score'''
+    '''Find the similarity score between two lists.
+
+    The function calculates the similarity score by multiplying each number in the left list
+    by the number of times it appears in the right list, and then summing these products.
+
+    Args:
+        left (list): The first list of integers.
+        right (list): The second list of integers.
+
+    Returns:
+        int: The similarity score between the two lists.
+    '''
     return sum(numb*right.count(numb) for numb in left)
 
 
@@ -27,3 +60,8 @@ if __name__ == '__main__':
     print('Task 1.1 Difference on input:', find_diff_id(*input_real))
     print('Task 1.2 Similarity on test:', find_similarity(*input_test))
     print('Task 1.2 Similarity on input:', find_similarity(*input_real))
+
+    # with open('input.txt', 'r', encoding='utf-8') as file:
+    #     left, right = zip(*(map(int, line.strip().split()) for line in file))
+    #     print('Part 1:', sum(abs(r_val - l_val) for l_val, r_val in zip(sorted(left), sorted(right))))
+    #     print('Part 2:', sum(numb*right.count(numb) for numb in left))
