59e106eca5f961707736ff54f22d928a0b95b28c
Tetiana Zakharchenko <smyarga@gmail.com>
2024-12-07 12:08:56 +0200

    Day 6. Documentation

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,6 +1,6 @@
 '''Day 6'''
 from itertools import cycle
-from copy import deepcopy
+
 
 def read_file(pathname: str) -> list[str]:
     """
@@ -12,41 +12,65 @@ def read_file(pathname: str) -> list[str]:
         pathname (str): The path to the file to be read.
 
     Returns:
-        list[str]: A list of strings, each representing a line from the file.
+        List[str]: A list of strings, each representing a line from the file.
     """
     with open(pathname, 'r', encoding='utf-8') as file:
         return file.read().splitlines()
 
-def main_function(rows: list[str]) -> int:
+
+def main_function(rows: list[str]) -> tuple[tuple[int, int], set[tuple[int, int]], int]:
+    """
+    Simulates a path traversal based on given directions and returns
+    the starting position, the set of visited positions, and the count
+    of unique positions visited.
+
+    Args:
+        rows (List[str]): A list of strings representing the grid with
+        directions.
+
+    Returns:
+        Tuple[Tuple[int, int], Set[Tuple[int, int]], int]: A tuple containing
+        the starting position, the set of visited positions, and the count of
+        unique positions visited.
+    """
     directions = {'^': (-1, 0), '>': (0, 1), '<': (0, -1), 'v': (1, 0)}
     turn = cycle('>v<^')
-    for i, row in enumerate(rows):
-        for j, el in enumerate(row):
-            if el == '^':
-                st_x, st_y = i, j
-    cur_x, cur_y = st_x, st_y
     cur_dir = '^'
+    for i, row in enumerate(rows):
+        if cur_dir in row:
+            st_x, st_y = i, row.index(cur_dir)
+    cur_x, cur_y = st_x, st_y
     path = set()
+
     while True:
         path.add((cur_x, cur_y))
         dx, dy = directions[cur_dir]
-        if 0 <= cur_x+dx < len(rows) and 0<= cur_y+dy < len(rows[0]) and rows[cur_x+dx][cur_y+dy] == '#':
-            cur_dir = next(turn)
-        elif 0 <= cur_x+dx < len(rows) and 0<= cur_y+dy < len(rows[0]):
-            cur_x += dx
-            cur_y += dy
+        next_x, next_y = cur_x + dx, cur_y + dy
+        if 0 <= next_x < len(rows) and 0 <= next_y < len(rows[0]):
+            if rows[next_x][next_y] == '#':
+                cur_dir = next(turn)
+            else:
+                cur_x, cur_y = next_x, next_y
         else:
             break
     return (st_x, st_y), path, len(set(path))
 
 
 def main_function2(rows: list[str]) -> int:
+    """
+    Determines the number of obstacles encountered when simulating path traversal
+    with different starting conditions.
+
+    Args:
+        rows (List[str]): A list of strings representing the grid with directions.
+
+    Returns:
+        int: The number of unique obstacles encountered.
+    """
     (st_x, st_y), path_all = main_function(rows)[:2]
     directions = {'^': (-1, 0), '>': (0, 1), '<': (0, -1), 'v': (1, 0)}
     obstacles = []
-    for el in set(path_all)-set((st_x, st_y)):
-        # n+=1
-        # print(el,n)
+    for el in set(path_all) - set((st_x, st_y)):
         turn = cycle('>v<^')
         rows_new = [list(row) for row in rows]
         rows_new[el[0]][el[1]] = '#'
@@ -56,17 +80,19 @@ def main_function2(rows: list[str]) -> int:
         while True:
             path.add((cur_x, cur_y, cur_dir))
             dx, dy = directions[cur_dir]
-            if 0 <= cur_x+dx < len(rows_new) and 0 <= cur_y+dy < len(rows_new[0]) and rows_new[cur_x+dx][cur_y+dy] == '#':
-                cur_dir = next(turn)
-            elif (cur_x+dx, cur_y+dy, cur_dir) in path:
-                obstacles.append((el[0], el[1]))
-                break
-            elif 0 <= cur_x+dx < len(rows_new) and 0 <= cur_y+dy < len(rows_new[0]):
-                cur_x += dx
-                cur_y += dy
+            next_x, next_y = cur_x + dx, cur_y + dy
+            if 0 <= next_x < len(rows_new) and 0 <= next_y < len(rows_new[0]):
+                if rows_new[next_x][next_y] == '#':
+                    cur_dir = next(turn)
+                elif (next_x, next_y, cur_dir) in path:
+                    obstacles.append((el[0], el[1]))
+                    break
+                else:
+                    cur_x, cur_y = next_x, next_y
             else:
                 break
     return len(set(obstacles))
+
 
 if __name__ == '__main__':
     import doctest
